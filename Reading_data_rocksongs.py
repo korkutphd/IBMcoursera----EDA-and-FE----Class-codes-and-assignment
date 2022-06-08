839771b1d77ed726e6fe1f6f268aab4c846e3300
Korkut <103966645+korkutphd@users.noreply.github.com>
2022-06-08 15:28:33 -0500

    Update Reading_data_rocksongs.py

--- Reading_data_rocksongs.py
+++ Reading_data_rocksongs.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
 
 # # Machine Learning Foundation
 # 
@@ -13,29 +11,8 @@ import sqlite3 as sq3
 import pandas.io.sql as pds
 import pandas as pd
 
-
-# ## Lab Exercise: Reading in database files
-# 
-#  - Create a variable, `path`, containing the path to the `baseball.db` contained in `resources/`
-#  - Create a connection, `con`, that is connected to database at `path`
-#  - Create a variable, `query`, containing a SQL query which reads in all data from the `allstarfull` table
-#  - Create a variable, `observations`, by using pandas' [read_sql](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_sql.html)
-# 
-# ### Optional
-#  - Create a variable, `tables`, which reads in all data from the table `sqlite_master`
-#  - Pretend that you were interesting in creating a new baseball hall of fame. Join and analyze the tables to evaluate the top 3 all time best baseball players.
-
-# In[5]:
-
-
-### BEGIN SOLUTION
-# Create a variable, `path`, containing the path to the `baseball.db` contained in `resources/`
 path = 'baseball.db'
-
-# Create a connection, `con`, that is connected to database at `path`
 con = sq3.Connection(path)
-
-# Create a variable, `query`, containing a SQL query which reads in all data from the `` table
 
 query = """
 SELECT *
@@ -45,7 +22,7 @@ SELECT *
 
 allstar_observations = pd.read_sql(query, con)
 
-# print(allstar_observations)
+print(allstar_observations)
 
 # Create a variable, tables, which reads in all data from the table sqlite_master
 all_tables = pd.read_sql('SELECT * FROM sqlite_master', con)
